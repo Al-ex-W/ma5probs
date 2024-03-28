bcdab4379ba4b2a6e58aad7ab0bf6a84c338883a
Alexander.Wahlström <alewah09@hotmail.com>
2024-03-28 17:47:33 +0100

    färdig kod v1

--- base.rb
+++ base.rb
@@ -1,16 +1,37 @@
+$numarr = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+$inputarr = []
+$numsum = 0
+$newvalue= ""
 def askfornum
-    puts "tal i bas 10?"
-    inputnum = gets.chomp.to_i
+    puts "tal?"
+    inputnum = gets.chomp
+    puts "vilken bas va det?"
+    inputbase = gets.chomp.to_i
     puts "Vilken bas ska det bli?"
     desiredbase = gets.chomp.to_i
-    #puts "vilken bas var det? svara med ett nummer"
-    #inputbase = gets.chomp.to_i
+    getvalue(inputnum,inputbase)
+    p "Ditt nya värde är: #{translatevalue(desiredbase)}"
 end
 
+def getvalue(num, base)
+    splittednum = num.chars.reverse
+    splittednum.each do |currentnum|
+        $inputarr << $numarr.index(currentnum)
+    end
+    i=0
+    $inputarr.each do |currentnum|
+        $numsum += (currentnum.to_i * (base ** i))
+        i += 1
+    end
+end
 
+def translatevalue(newbase)
+    while ($numsum / newbase) >= 1
+       $newvalue += $numarr[($numsum % newbase)]
+       $numsum = $numsum / newbase
+    end
+    $newvalue += $numarr[($numsum % newbase)]
+    $newvalue.reverse!
+end
 
-
-def getvalue(num)
-    numarr = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    if numarr[num].type == string
-
+askfornum
