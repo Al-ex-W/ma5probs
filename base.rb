d99827463c77b85b0a55a3893010defc154c405f
Alexander.Wahlström <alewah09@hotmail.com>
2024-04-07 22:55:32 +0200

    går och lägger mig

--- base.rb
+++ base.rb
@@ -32,7 +32,7 @@ def getvalue(num, base) # översätter tal från vilken bas som helst till 10 ba
 
     # nu är $inputarr en array, varje värde i den ska nu översättas och slås ihop till ett tal med tiobas.
 
-    i = (0-amount_of_decimals) # Startvärde för exponenten, ökar för varje nytt värde från $inputarr, hänvisa till pappret.
+    i = (0-amount_of_decimals) # Startvärde för exponenten, ökar för varje nytt värde från $inputarr, hänvisa till pappret del 1.
     $inputarr.each do |currentnum| # varje värde i inputarr ska ny översättas till sitt tiobas värde
         $numsum += (currentnum.to_i * (base ** i)) # Räknar ut talets värde i bas 10 och sedan adderar det på det slutliga talet ($numsum) i bas 10, genom att ta 10 upphöjt med i, som är den exponenten som nuvarande värdet ska ha, vilket var därför vi reversade, så att lägsta tallet kommer först och har exponenten 0.
         i += 1 # Ökar exponenten för varje värde
@@ -47,9 +47,11 @@ def translatevalue(newbase) # Översätter tal i tiobas till vad som helst
         all_decimals = numtest[1].to_i
     end
 
-    while ($numsum / newbase) >= 1 # kollar om $numsum går att representera med (ytterligare) 1 tecken från den nya basen, om den behöver fler, ska den köra loopen, hänvisa till pappret.
-       $newvalue += $numarr[($numsum % newbase)] # konkatenerar $newvalue (talet i den nya basen) med resten som blir när talet i tiobas delas med den nya basen, hänvisa till papper. 
-       $numsum = $numsum / newbase # delar numsum med basen för att sedan fortsätta på nästa tal i den nya basen, hänvisa till papper
+    #HELTAL!!!
+
+    while ($numsum / newbase) >= 1 # kollar om $numsum går att representera med (ytterligare) 1 tecken från den nya basen, om den behöver fler, ska den köra loopen, hänvisa till pappret del 2.
+       $newvalue += $numarr[($numsum % newbase)] # konkatenerar $newvalue (talet i den nya basen) med resten som blir när talet i tiobas delas med den nya basen, hänvisa till papper del 2. 
+       $numsum = $numsum / newbase # delar numsum med basen för att sedan fortsätta på nästa tal i den nya basen, hänvisa till papper del 2
     end
     $newvalue += $numarr[($numsum % newbase)] # Hanterar sista värdet, körs ej i loopen
     $newvalue.reverse! # Vänder talet så att den högsta positionen kommer först
@@ -57,12 +59,12 @@ def translatevalue(newbase) # Översätter tal i tiobas till vad som helst
     #DECIMALER??
     
     if all_decimals != 0 #Har talet decimaler?
-        all_decimals = ("0." + "#{all_decimals}").to_f # gör om decimalerna till riktiga decimaler i float.
+        all_decimals = ("0." + "#{all_decimals}").to_f # gör om decimalerna till riktiga decimaler i float. -> 0.B6U
         i=0 # i håller räkningen på antal loopningnar här nere
         newdecimals = "" 
         while all_decimals != 0 && i < 6 #om det finns decimaler kvar att beräkna eller om den har loopat mindre än 6 gånger så körs loopen
-            newdecimals << $numarr[((all_decimals * newbase).to_i)] # multiplicerar (de återstående) decimelerna. Heltalet som blir som resultat är decimalens värde. Tecknet för detta värde hämtas ur $numarr, se papper.
-            all_decimals = all_decimals * newbase - ((all_decimals * newbase).to_i) # sätter all_decimals till det återstående värdet på decimaltalen, som måste visas med ännu mindre decimaler, se papper
+            newdecimals << $numarr[((all_decimals * newbase).to_i)] # multiplicerar (de återstående) decimelerna. Heltalet som blir som resultat är decimalens värde. Tecknet för detta värde hämtas ur $numarr, se papper del 3.
+            all_decimals = all_decimals * newbase - ((all_decimals * newbase).to_i) # sätter all_decimals till det återstående värdet på decimaltalen, som måste visas med ännu mindre decimaler, se papper del 3
             i+=1
         end
         $newvalue << ".#{newdecimals}"
